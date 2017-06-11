c97c73153ce4c5d565df17682de1bdee9a3caf7d
Priyanka Advani <padvani89@gmail.com>
2017-06-11 18:38:54 +0000

    Completed skills assessment

---
+++ server.py
@@ -0,0 +1,60 @@
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from jinja2 import StrictUndefined
+
+
+app = Flask(__name__)
+app.jinja_env.undefined = StrictUndefined
+app.jinja_env.auto_reload = True
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "\xdd~\x08Ry\x16~|\n\xba\xa5\x86\x9e\xe3AX\xaf@?\xa8\xd9zA\xe9"
+
+
+@app.route("/")
+def index():
+
+    return render_template("index.html")
+
+
+@app.route("/application-form")
+def application_form():
+
+    return render_template("application-form.html")
+
+
+@app.route("/application-success", methods=["POST"])
+def application_success():
+
+    firstname = request.form.get("firstname")
+    lastname = request.form.get("lastname")
+    name = "{} {}".format(firstname, lastname)
+
+
+    position = request.form.get("position")
+    if position == "qa_engineer":
+        job = position.split("_")
+        job[0] = job[0].upper()
+        job[1] = job[1].capitalize()
+        position = " ".join(job)
+    else:
+        position = " ".join(position.split("_")).title()
+
+    salary = request.form.get("salary")
+    salary = "${:6,}".format(int(salary))
+
+    return render_template("application-response.html",
+                            name=name,
+                            position=position,
+                            salary=salary)
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0")
